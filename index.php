3f9cbc8ec2440b18bf7a933c2a998b53544ef576
ManhProduce <thieusymanh2001@gmail.com>
2023-01-30 14:52:18 +0700

    index

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>M&T medical supplies & services</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="left-test col-3 bg-primary">
+                left
+            </div>
+            <div class="right-test col-9 bg-danger">
+                right
+            </div>
+        </div>
+    </div>
+</body>
+</html>
