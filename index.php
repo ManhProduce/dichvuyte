057b67107885a0895a10e56557a21bc728bf9c18
ManhProduce <thieusymanh2001@gmail.com>
2023-01-31 17:03:52 +0700

    làm header
    làm phần top header index.php

--- index.php
+++ index.php
@@ -7,19 +7,56 @@
     <title>M&T medical supplies & services</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css">
+    <link rel="stylesheet" href="css/index_style.css">
 </head>
 <body>
-    <div class="container">
-        <div class="header">
-            header
+    <div class="wrapper">
+        <div id="header">
+            <div class="top-header">
+                <div class="container">
+                    <p class="open-hour">Giờ làm việc: Thứ Hai - Thứ Sáu: 8:00 - 16:30 - Thứ Bảy: 8:00 - 11:30</p>
+                    <ul class="social-nav">
+                        <li class="social-nav-item">
+                            <a href="#" class="social-nav-item-link"><i class="fa-brands fa-facebook"></i></a>
+                        </li>
+                        <li class="social-nav-item">
+                            <a href="#" class="social-nav-item-link"><i class="fa-brands fa-google-plus-g"></i></a>
+                        </li>
+                        <li class="social-nav-item">
+                            <a href="#" class="social-nav-item-link"><i class="fa-brands fa-instagram"></i></a>
+                        </li>
+                        <li class="social-nav-item">
+                            <a href="#" class="social-nav-item-link"><i class="fa-brands fa-linkedin"></i></a>
+                        </li>
+                        <ul class="social-nav-item nav-dropdown">
+                            <a href="#" class="social-nav-item-link"><i class="fa-sharp fa-solid fa-caret-down"></i></a>
+                            <div class="nav-dropdown-menu">
+                                <li class="nav-dropdown-item">
+                                    <a href="#" class="social-nav-item-link">
+                                        <i class="fa-solid fa-right-to-bracket"></i>
+                                        Log in
+                                    </a>
+                                </li>
+                                <li class="nav-dropdown-item">
+                                    <a href="#" class="social-nav-item-link">
+                                        <i class="fa-solid fa-right-from-bracket"></i>
+                                        Log out
+                                    </a>
+                                </li>
+                            </div>
+                        </ul>
+                    </ul>
+                </div>
+            </div>
         </div>
-        <div class="banner">
+        <div id="banner">
             banner
         </div>
-        <div class="body">
+        <div id="body">
             body
         </div>
-        <div class="footer">
+        <div id="footer">
             footer
         </div>
     </div>
