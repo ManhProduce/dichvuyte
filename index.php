c83f0657e4ce36d7daba4324acc7020c56d1cb32
ManhProduce <thieusymanh2001@gmail.com>
2023-05-10 11:46:50 +0700

    ...

--- index.php
+++ index.php
@@ -16,6 +16,12 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>M&T medical supplies & services</title>
+    <meta name="description" content="Dịch vụ y tế>">
+    <meta name="description" content="Vật tư y tế>">
+	<meta name="keywords" content="Dịch vụ">
+	<meta name="keywords" content="Vật tư">
+	<meta name="keywords" content="Y tế">
+	<meta name="keywords" content="M&T">
     <!-- link bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
